a69dc12ba460b0135c2467c0240db29ac885857a
xiezhixian <268022625@qq.com>
2022-05-20 17:07:41 +0800

    添加2个新文档，修改了注册的静态页面为动态

---
+++ EasyContracts/src/main/java/bjtu/pt/easycontracts/controller/SkipController.java
@@ -0,0 +1,10 @@
+package bjtu.pt.easycontracts.controller;/**
+ * <Description> SkipController
+ * @author 26802
+ * @version 1.0
+ * @ClassName SkipController
+ * @taskId 
+ * @see bjtu.pt.easycontracts.controller 
+ */
+public class SkipController {
+}
