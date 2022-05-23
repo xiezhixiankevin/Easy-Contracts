f4ba65c25b5fbd8e46eb44b62aac0eddc669c68e
zhangxincheng <1449722625@qq.com>
2022-05-23 17:35:11 +0800

    插入数据d文件

---
+++ insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO user VALUES
+(1,'zxc','BOSS','password','1449722625@qq.com'),
+(2,'xzx','servant1','password','1449722626@qq.com'),
+(3,'hcb','servant2','password','1449722627@qq.com'),
+(4,'wj','servant3','password','1449722628@qq.com'),
+(5,'fbf','servant4','password','1449722629@qq.com');
+
+INSERT INTO customer VALUES
+(1,'zxc','sky','123123123','123123','111','zxc'),
+(2,'xzx','dust','123456789','123123','222','xzx'),
+(3,'hcb','sea','123456788','123123','333','hcb'),
+(4,'wj','tree','123456787','123123','444','wj'),
+(5,'fbf','underground','123456786','123123','555','fbf');
+
+INSERT INTO rights VALUES
+(1,'aaa','aaaaaaaaaaaaaaaaaaa','AAAAAAAA'),
+(2,'bbb','bbbbbbbbbbbbbbbbbbb','BBBBBBBB'),
+(3,'ccc','ccccccccccccccccccc','CCCCCCCC'),
+(4,'ddd','ddddddddddddddddddd','DDDDDDDD'),
+(5,'eee','eeeeeeeeeeeeeeeeeee','EEEEEEEE');
