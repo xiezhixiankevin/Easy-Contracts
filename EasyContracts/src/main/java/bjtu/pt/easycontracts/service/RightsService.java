0893c9e9e6483491ba37ebbb583b2eca153bd39a
xiezhixian <268022625@qq.com>
2022-05-23 10:49:52 +0800

    xzx

--- EasyContracts/src/main/java/bjtu/pt/easycontracts/service/RightsService.java
+++ EasyContracts/src/main/java/bjtu/pt/easycontracts/service/RightsService.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public interface RightsService {
 
-    //分配角色(考虑到前端分配和修改时是选择框一次性提交，而不是一个个删或者改，所以每次分配之前记得把这个人的所有已有的角色删完再分配)
+    //分配一组权限，可以调用allocationRights方法(考虑到前端分配和修改时是选择框一次性提交，而不是一个个删或者改，所以每次分配之前记得把这个人的所有已有的角色删完再分配)
     int allocationRights(int userId, List<Rights> rights);
 
     //分配一个权限
