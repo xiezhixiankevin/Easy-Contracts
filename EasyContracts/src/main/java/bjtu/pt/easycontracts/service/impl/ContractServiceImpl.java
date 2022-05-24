8a4e8471b53f92524b87e87456d4fdcd1d7a20e2
zhangxincheng <1449722625@qq.com>
2022-05-24 19:48:35 +0800

    5.24zxc

--- EasyContracts/src/main/java/bjtu/pt/easycontracts/service/impl/ContractServiceImpl.java
+++ EasyContracts/src/main/java/bjtu/pt/easycontracts/service/impl/ContractServiceImpl.java
@@ -1,17 +1,24 @@
 package bjtu.pt.easycontracts.service.impl;
 
 import bjtu.pt.easycontracts.mapper.ContractMapper;
-import bjtu.pt.easycontracts.pojo.table.Contract;
-import bjtu.pt.easycontracts.pojo.table.ContractExample;
+import bjtu.pt.easycontracts.mapper.ContractProcessMapper;
+import bjtu.pt.easycontracts.pojo.table.*;
+import bjtu.pt.easycontracts.service.ContractProcessService;
 import bjtu.pt.easycontracts.service.ContractService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.SimpleTimeZone;
+
+import static bjtu.pt.easycontracts.utils.Global.*;
 
 public class ContractServiceImpl implements ContractService
 {
     @Autowired
     private ContractMapper contractMapper;
+    @Autowired
+    private ContractProcessMapper contractProcessMapper;
 
     @Override
     public List<Contract> listContractSelective(Contract contract)
@@ -46,7 +53,8 @@ public class ContractServiceImpl implements ContractService
 
     @Override
     public int addContract(Contract contract) {
-        return 0;
+        contractMapper.insert(contract);
+        return SUCCESS;
     }
 
     @Override
@@ -59,7 +67,9 @@ public class ContractServiceImpl implements ContractService
 
     @Override
     public int updateContract(int id, Contract newContract) {
-        return 0;
+        deleteContract(id);
+        addContract(newContract);
+        return SUCCESS;
     }
 
     @Override
@@ -69,7 +79,64 @@ public class ContractServiceImpl implements ContractService
 
     @Override
     public int examineConTract(int contractId, int userId, String opinion, boolean ifPass) {
-        return 0;
+        ContractProcessServiceImpl contractProcessServiceImpl=new ContractProcessServiceImpl();
+        ContractProcess contractProcess=new ContractProcess();
+
+        //Date
+        java.util.Date date=new java.util.Date();
+        contractProcess.setTime(date);
+
+        contractProcess.setContractid(contractId);
+        contractProcess.setContent(opinion);
+        contractProcess.setUserid(userId);
+        if(ifPass==true){
+            contractProcess.setState(PASS);
+        }else{
+            contractProcess.setState(VETO);
+        }
+        contractProcessServiceImpl.updateProcess(userId,contractId,contractProcess);
+
+        //检查
+        ContractProcessExample contractProcessExample=new ContractProcessExample();
+        contractProcessExample.createCriteria().andContractidEqualTo(contractId).andTypeEqualTo(EXAM);
+        List <ContractProcess> contractProcessList = contractProcessMapper.selectByExample(contractProcessExample);
+        int flag1=0;//记录操作次数
+        int flag2=0;//记录PASS次数
+        for(int i=0;i<contractProcessList.size();i++){
+            if(contractProcessList.get(i).getState()==PASS  ||contractProcessList.get(i).getState()== VETO)
+            {
+                flag1++;
+                if(contractProcessList.get(i).getState()==PASS){
+                    flag2++;
+                }
+            }
+        }
+        if(flag2==contractProcessList.size()){
+            Contract contract=contractMapper.selectByPrimaryKey(contractId);
+            contract.setType(SIGNING);//进入下一阶段签订
+            ContractExample contractExample=new ContractExample();
+            contractExample.createCriteria().andContractidEqualTo(contractId);
+            contractMapper.updateByExample(contract,contractExample);
+            return SUCCESS;
+        }else if(flag1==contractProcessList.size()){
+            //将合同进程表里数据 status改成0
+            ContractProcessExample contractProcessExample2=new ContractProcessExample();
+            contractProcessExample.createCriteria().andContractidEqualTo(contractId).andTypeEqualTo(EXAM);
+            List <ContractProcess> contractProcessList2 = contractProcessMapper.selectByExample(contractProcessExample2);
+            for(int k=0;k<contractProcessList2.size();k++){
+                contractProcessList2.get(k).setState(NOT_COME);
+                contractProcessServiceImpl.updateProcess(userId,contractId,contractProcessList2.get(k));
+            }
+            //
+            Contract contract=contractMapper.selectByPrimaryKey(contractId);
+            contract.setType(FINALIZING);//重新回到定稿阶段
+            contract.setFailuretimes(contract.getFailuretimes()+1);
+            ContractExample contractExample=new ContractExample();
+            contractExample.createCriteria().andContractidEqualTo(contractId);
+            contractMapper.updateByExample(contract,contractExample);
+            return SUCCESS;
+        }
+        return SUCCESS;
     }
 
     @Override
