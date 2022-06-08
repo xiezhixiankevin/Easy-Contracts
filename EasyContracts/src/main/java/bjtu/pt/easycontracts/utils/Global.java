6026c4bc83f83c58c3a509a1f5d8fac618a763d0
xiezhixian <268022625@qq.com>
2022-06-08 22:06:59 +0800

    新增生产版本的文件

--- EasyContracts/src/main/java/bjtu/pt/easycontracts/utils/Global.java
+++ EasyContracts/src/main/java/bjtu/pt/easycontracts/utils/Global.java
@@ -82,5 +82,6 @@ public class Global {
 
 
     //文件上传路径
-    public static final String FILE_PATH = "F://files";
+//    public static final String FILE_PATH = "F://files"; //windows
+    public static final String FILE_PATH = "/home/project/EasyContract/files"; //linux
 }
